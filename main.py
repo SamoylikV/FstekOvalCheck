5393fefcbabac6face30956ce3c8f8ff5b139ceb
Vasiliy Samoylik <44618834+SamoylikV@users.noreply.github.com>
2024-04-02 19:01:43 +0400

    log fixed
    minor changes

--- main.py
+++ main.py
@@ -11,10 +11,10 @@ handler.setFormatter(colorlog.ColoredFormatter(
     datefmt=None,
     reset=True,
     log_colors={
-        'DEBUG':    'cyan',
-        'INFO':     'green',
-        'WARNING':  'yellow',
-        'ERROR':    'red',
+        'DEBUG': 'cyan',
+        'INFO': 'green',
+        'WARNING': 'yellow',
+        'ERROR': 'red',
         'CRITICAL': 'red,bg_white',
     },
     secondary_log_colors={},
@@ -27,7 +27,7 @@ logger.addHandler(handler)
 
 
 def get_cve_ids_from_export(export_file_path):
-    logging.info(f"Начинаем извлечение идентификаторов CVE из {export_file_path}")
+    logger.info(f"Начинаем извлечение идентификаторов CVE из {export_file_path}")
     cve_ids = set()
     try:
         tree = ET.parse(export_file_path)
@@ -36,66 +36,66 @@ def get_cve_ids_from_export(export_file_path):
         for identifier in identifiers:
             if identifier.get('type') == "CVE":
                 cve_ids.add(identifier.text)
-        logging.info(f"Успешно извлечено {len(cve_ids)} идентификаторов CVE.")
+        logger.info(f"Успешно извлечено {len(cve_ids)} идентификаторов CVE.")
     except ET.ParseError as e:
-        logging.error(f"Ошибка при разборе XML: {e}")
+        logger.error(f"Ошибка при разборе XML: {e}")
     except Exception as e:
-        logging.error(f"Произошла ошибка: {e}")
+        logger.error(f"Произошла ошибка: {e}")
     return list(cve_ids)
 
 
 def merge_oval_files(files_list, output_file_path="CVE.FSTEK.xml"):
-    logging.info("Начинаем слияние OVAL файлов...")
+    logger.info("Начинаем слияние OVAL файлов...")
     if not files_list or len(files_list) < 2:
-        logging.warning("Для слияния требуется минимум два файла.")
+        logger.warning("Для слияния требуется минимум два файла.")
         return
     tree1 = etree.parse(files_list[0])
     root1 = tree1.getroot()
     for file_path in files_list[1:]:
-        logging.info(f"Обработка файла {file_path}")
+        logger.info(f"Обработка файла {file_path}")
         tree = etree.parse(file_path)
         vulnerabilities = tree.xpath('//vulnerability')
         for vulnerability in vulnerabilities:
             root1.append(vulnerability)
     tree1.write(output_file_path, pretty_print=True, xml_declaration=True, encoding="UTF-8")
-    logging.info(f"Файлы успешно объединены и сохранены в {output_file_path}")
+    logger.info(f"Файлы успешно объединены и сохранены в {output_file_path}")
     return files_list[1:]
 
 
 def make_commands(export_file_path):
-    logging.info(f"Начинаем создание команд для экспорта CVE из {export_file_path}")
+    logger.info(f"Начинаем создание команд для экспорта CVE из {export_file_path}")
     cve_ids = get_cve_ids_from_export(export_file_path)
     cve_ids_final = []
     for cve_id in cve_ids:
         if cve_id.startswith("CVE") and len(cve_id.split('-')[1]) == 4 and len(cve_id.split('-')[2]) >= 4:
             cve_ids_final.append(cve_id)
-    logging.info(f"Отфильтровано {len(cve_ids_final)} подходящих идентификаторов CVE.")
+    logger.info(f"Отфильтровано {len(cve_ids_final)} подходящих идентификаторов CVE.")
     counter = 0
     counter2 = 1
     files = []
     with open('oval_make.command', 'w') as file:
-        logging.info("Начинаем запись команд в файл.")
+        logger.info("Начинаем запись команд в файл.")
         for cve_id in cve_ids_final:
             counter += 1
             if counter % 5000 == 0:
                 counter2 += 1
                 files.append(f"CVE.FSTEK{counter2}.xml")
                 file.write("\n")
-                logging.info(f"Создание команды для файла CVE.FSTEK{counter2}.xml")
+                logger.info(f"Создание команды для файла CVE.FSTEK{counter2}.xml")
                 file.write(
                     f"python3 {os.path.dirname(os.path.realpath(__file__))}/OVALRepo/scripts/build_oval_definitions_file.py -o {os.path.dirname(os.path.realpath(__file__))}/CVE.FSTEK{counter2}.xml --family unix --reference_id ")
             elif counter == 1:
                 files.append(f"CVE.FSTEK{counter2}.xml")
-                logging.info("Создание начальной команды для экспорта.")
+                logger.info("Создание начальной команды для экспорта.")
                 file.write(
                     f"python3 {os.path.dirname(os.path.realpath(__file__))}/OVALRepo/scripts/build_oval_definitions_file.py -o {os.path.dirname(os.path.realpath(__file__))}/CVE.FSTEK{counter2}.xml --family unix --reference_id ")
             file.write(f'"{cve_id}" ')
-    logging.info("Команды успешно записаны.")
+    logger.info("Команды успешно записаны.")
     return files, f"{os.path.dirname(os.path.realpath(__file__))}"
 
 
 def execute_command(command):
-    if len(command) > 100 and not command.startswith("oscap"):
+    if len(command) > 100:
         logger.info(f"Выполнение команды: {command[:100]}...")
     else:
         logger.info(f"Выполнение команды: {command}")
@@ -142,14 +142,13 @@ def main():
 
     merge_oval_files(files)
 
-    logger.info("Выполнение оценки уязвимостей...")
-    execute_command(
-        f"oscap oval eval --results {os.path.join(path, 'results.xml')} --report report.html {os.path.join(path, 'CVE.FSTEK.xml')}")
-    logger.info("Оценка завершена.")
+    # logger.info("Выполнение оценки уязвимостей...")
+    # execute_command(
+    #     f"oscap oval eval --results {os.path.join(path, 'results.xml')} --report report.html {os.path.join(path, 'CVE.FSTEK.xml')}")
+    # logger.info("Оценка завершена.")
     logger.info("Удаление временных файлов...")
     for file in files:
         execute_command(f"rm {file}")
-    execute_command(f"rm {os.path.join(path, 'CVE.FSTEK.xml')}")
     execute_command(f"rm {os.path.join(path, 'oval_make.command')}")
     execute_command(f"rm {os.path.join(path, 'export.xml')}")
     execute_command(f"rm -rf {os.path.join(path, 'OVALRepo')}")
